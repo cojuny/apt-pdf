afd5517d4c9bd8e54611b994be9175bca05b3cbd
Junyoung Wang <codingdaniel0205@outlook.com>
2024-01-08 13:24:38 +0800

    pytest fix

--- SearchEngine/tests/test_HelloWorld.py
+++ SearchEngine/tests/test_HelloWorld.py
@@ -1,11 +1,6 @@
-import unittest
 from src.HelloWorld import HelloWorld
 
-class TestHelloWorld(unittest.TestCase):
-    def test_get_message(self):
-        hw = HelloWorld()
-        message = hw.hello_world()
-        self.assertEqual(message, "Hello, World!")
+def test_hello_world():
+    hw = HelloWorld()
+    assert hw.hello_world() == "Hello, World!"
 
-if __name__ == '__main__':
-    unittest.main()
