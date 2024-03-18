5c0eba32b2626dfb0f08b2d23eecccd8a7afa3b4
Junyoung Wang <codingdaniel0205@outlook.com>
2024-03-18 17:58:47 +0800

    __init__ update

--- SearchEngine/src/__init__.py
+++ SearchEngine/src/__init__.py
@@ -1,7 +1,7 @@
 import nltk
 
 def download_nltk_resources():
-    resources = ["punkt", "wordnet", "averaged_perceptron_tagger", "stopwords"]  # Add other NLTK resources as needed
+    resources = ["punkt", "wordnet", "averaged_perceptron_tagger", "stopwords", "universal_tagset", "brown"]  # Add other NLTK resources as needed
     for resource in resources:
         try:
             nltk.data.find(resource)
